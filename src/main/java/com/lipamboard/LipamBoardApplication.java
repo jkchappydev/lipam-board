740fda47ef57bcfda9fe9d5985cb678e02eac619
jkchappydev <jkchappydev@gmail.com>
2025-09-23 02:30:48 +0900

    build: 프로젝트 설정

---
+++ src/main/java/com/lipamboard/LipamBoardApplication.java
@@ -0,0 +1,13 @@
+package com.lipamboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LipamBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LipamBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lipamboard/LipamBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lipamboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LipamBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
