dec85afe289d4d5d026db9a576845e2a7aaf0697
jkchappydev <jkchappydev@gmail.com>
2026-02-15 19:59:58 +0900

    feat: 좋아요 CRUD 구현 + Test 작성

---
+++ service/like/src/main/java/lipam/board/like/controller/ArticleLikeController.java
@@ -0,0 +1,38 @@
+package lipam.board.like.controller;
+
+import lipam.board.like.service.ArticleLikeService;
+import lipam.board.like.service.response.ArticleLikeResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class ArticleLikeController {
+
+    private final ArticleLikeService articleLikeService;
+
+    @GetMapping("/v1/article-likes/articles/{articleId}/users/{userId}")
+    public ArticleLikeResponse read(
+            @PathVariable("articleId") Long articleId,
+            @PathVariable("userId") Long userId
+    ) {
+        return articleLikeService.read(articleId, userId);
+    }
+
+    @PostMapping("/v1/article-likes/articles/{articleId}/users/{userId}")
+    public void like(
+            @PathVariable("articleId") Long articleId,
+            @PathVariable("userId") Long userId
+    ) {
+        articleLikeService.like(articleId, userId);
+    }
+
+    @DeleteMapping("/v1/article-likes/articles/{articleId}/users/{userId}")
+    public void unlike(
+            @PathVariable("articleId") Long articleId,
+            @PathVariable("userId") Long userId
+    ) {
+        articleLikeService.unlike(articleId, userId);
+    }
+
+}
---
+++ service/like/src/main/java/lipam/board/like/service/ArticleLikeService.java
@@ -0,0 +1,44 @@
+package lipam.board.like.service;
+
+import lipam.board.common.snowflake.Snowflake;
+import lipam.board.like.entity.ArticleLike;
+import lipam.board.like.repository.ArticleLikeRepository;
+import lipam.board.like.service.response.ArticleLikeResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleLikeService {
+
+    private final Snowflake snowflake = new Snowflake();
+    private final ArticleLikeRepository articleLikeRepository;
+
+    public ArticleLikeResponse read(
+            Long articleId,
+            Long userId
+    ) {
+        return articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
+                .map(ArticleLikeResponse::from)
+                .orElseThrow();
+    }
+
+    @Transactional
+    public void like(Long articleId, Long userId) {
+        articleLikeRepository.save(
+                ArticleLike.create(
+                        snowflake.nextId(),
+                        articleId,
+                        userId
+                )
+        );
+    }
+
+    @Transactional
+    public void unlike(Long articleId, Long userId) {
+        articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
+                .ifPresent(articleLikeRepository::delete);
+    }
+
+}
