d613346293bd52e0a5d88ae7c8455c0961a7386e
jkchappydev <jkchappydev@gmail.com>
2026-02-18 19:33:02 +0900

    feat: 댓글 수 구현

--- service/comment/src/main/java/lipam/board/comment/service/CommentServiceV2.java
+++ service/comment/src/main/java/lipam/board/comment/service/CommentServiceV2.java
@@ -1,7 +1,9 @@
 package lipam.board.comment.service;
 
+import lipam.board.comment.entity.ArticleCommentCount;
 import lipam.board.comment.entity.CommentPath;
 import lipam.board.comment.entity.CommentV2;
+import lipam.board.comment.repository.ArticleCommentCountRepository;
 import lipam.board.comment.repository.CommentRepositoryV2;
 import lipam.board.comment.service.request.CommentCreateRequestV2;
 import lipam.board.comment.service.response.CommentPageResponse;
@@ -21,6 +23,7 @@ public class CommentServiceV2 {
 
     private final Snowflake snowflake = new Snowflake();
     private final CommentRepositoryV2 commentRepository;
+    private final ArticleCommentCountRepository articleCommentCountRepository;
 
     @Transactional
     public CommentResponse create(CommentCreateRequestV2 request) {
@@ -39,6 +42,14 @@ public class CommentServiceV2 {
                         )
                 )
         );
+
+        // 댓글이 생성될 때, 전체 댓글 수 + 1
+        int result = articleCommentCountRepository.increase(request.getArticleId());
+        if (result == 0) {
+            articleCommentCountRepository.save(
+                    ArticleCommentCount.init(request.getArticleId(), 1L)
+            );
+        }
 
         return CommentResponse.from(comment);
     }
@@ -79,7 +90,7 @@ public class CommentServiceV2 {
 
     private void delete(CommentV2 comment) {
         commentRepository.delete(comment); // 댓글을 DB 에서 실제 삭제
-
+        articleCommentCountRepository.decrease(comment.getArticleId()); // 댓글이 실제 삭제될 때, 전체 댓글 수 - 1
         if (!comment.isRoot()) { // 삭제 대상이 대댓글이면, 부모도 정리 대상인지 확인
             commentRepository.findByPath(comment.getCommentPath().getPath()) // 부모 댓글 조회
                     .filter(CommentV2::getDeleted) // 1. 부모가 이미 "삭제 표시" 상태인 경우만
@@ -98,6 +109,7 @@ public class CommentServiceV2 {
                         .map(CommentResponse::from)
                         .toList(),
                 commentRepository.count(articleId, PageLimitCalculator.calculatePageLimit(page, pageSize, 10L))
+                // count(articleId) // 예전엔 이동한 페이지에 대한 댓글 수를 일부만 카운트했지만, 이제 댓글 수를 실시간으로 바로 조회하니까 count(articleId)로도 바꿀 수 있다.
         );
     }
 
@@ -109,6 +121,12 @@ public class CommentServiceV2 {
         return comments.stream()
                 .map(CommentResponse::from)
                 .toList();
+    }
+
+    public Long count(Long articleId) {
+        return articleCommentCountRepository.findById(articleId)
+                .map(ArticleCommentCount::getCommentCount)
+                .orElse(0L);
     }
 
 }
